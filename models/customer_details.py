fb2cbea979453a590ecbdbe40e95b8ef3add3bf5
ian <ian@intellisoftplus.com>
2016-05-27 13:11:12 +0300

    emails
    Form can send emails on submission

--- models/customer_details.py
+++ models/customer_details.py
@@ -1,6 +1,8 @@
 from google.appengine.ext import ndb
 from google.appengine.api import search
 
+from google.appengine.api import mail
+
 class customer_details(ndb.Model):
     names = ndb.StringProperty()
     email = ndb.StringProperty()
@@ -18,5 +20,23 @@ class customer_details(ndb.Model):
     		org = org,
     		number = number,
     		product = product
-    		
+
     		).put()
+
+    	#send an email to sales team
+    	message = mail.EmailMessage(sender="ISP Marketting Campaign <noreply@isplandingpage.appspotmail.com>",subject="NEW SIGN UP - REQUIRES YOUR ATTENTION")
+    	message.to = "chantal@intellisoftplus.com,wakari@intellisoftplus.com,reshiwani@intellisoftplus.com"
+    	message.body = """
+				New sign up,
+
+				The below contact has filled the report form.
+
+				Name: %s
+				Phone No.: %s
+				E mail : %s
+				Product : %s
+
+				Please follow up immediately and Slack the team to keep them in the loop.
+				""" % (names,number,email, product)
+        message.send()
+
